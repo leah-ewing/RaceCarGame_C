9f35c36f57f37f6e7f50f5aafa32b71eaeb89a24
Leah Ewing <leahewing@live.com>
2022-03-03 08:06:27 +0000

    add comments

--- raceCarGame.c
+++ raceCarGame.c
@@ -57,40 +57,38 @@ struct RaceCar {
 
 void printIntro() {
   printf("Welcome to our main event digital race fans!\nI hope everybody has their snacks because we are about to begin!\n\n");
-};
+}; // Prints opening greeting
 
 void printCountDown() {
   printf("Racers Ready! In...\n5\n4\n3\n2\n1\nRace!\n\n");
-};
+}; // Prints race countdown
 
 void printFirstPlaceAfterLap(struct Race race) {
   int currentLap = race.currentLap;
   char* firstPlaceDriverName = race.firstPlaceDriverName;
   char* firstPlaceRaceCarColor = race.firstPlaceRaceCarColor;
   printf("After lap %i\nFirst Place Is: %s in the %s race car!\n\n", currentLap, firstPlaceDriverName, firstPlaceRaceCarColor);
-};
+}; // Print statement for each lap with the current lap number, the first place driver, and the first place car color
 
 void printCongratulation(struct Race race) {
   char* firstPlaceDriverName = race.firstPlaceDriverName;
   char* firstPlaceRaceCarColor = race.firstPlaceRaceCarColor;
-  printf("Let's all congratulate %s in the %s race car for an amazing performance.\n\nIt truly was a great race and everybody have a goodnight!\n\n", firstPlaceDriverName, firstPlaceRaceCarColor);
-};
+  printf("Let's all congratulate %s in the %s race car for an amazing performance.\n\nIt truly was a great race and everybody have a good night!\n\n", firstPlaceDriverName, firstPlaceRaceCarColor);
+}; // Prints winner's statement with winning driver and winning car color
 
 int calculateTimeToCompleteLap() {
   int speed = rand() % 3 + 1;
   int acceleration = rand() % 3 + 1;
   int nerves = rand() % 3 + 1;
-
   return speed + acceleration + nerves;
-};
+}; // Calculates lap times by the sum of randomizing variables speed, acceleration, and nerves on a scale of 1-3
 
 void updateRaceCar(struct RaceCar* raceCar) {
   int lapTime = calculateTimeToCompleteLap();
   (*raceCar).totalLapTime += lapTime;
-};
+}; // Updates the total lap times for the driver by adding their most recent lap time
 
 void updateFirstPlace(struct Race* race, struct RaceCar* raceCar1, struct RaceCar* raceCar2) {
-
   if ((*raceCar1).totalLapTime <= (*raceCar2).totalLapTime) {
     strcpy((*race).firstPlaceDriverName, (*raceCar1).driverName);
     strcpy((*race).firstPlaceRaceCarColor, (*raceCar1).raceCarColor);
@@ -98,8 +96,7 @@ void updateFirstPlace(struct Race* race, struct RaceCar* raceCar1, struct RaceCa
     strcpy((*race).firstPlaceDriverName, (*raceCar2).driverName);
     strcpy((*race).firstPlaceRaceCarColor, (*raceCar2).raceCarColor);
   }
-
-};
+}; // Updates the first place position's driver name and car color
 
 void startRace(struct RaceCar* raceCar1, struct RaceCar* raceCar2) {
   struct Race race = {
@@ -107,7 +104,7 @@ void startRace(struct RaceCar* raceCar1, struct RaceCar* raceCar2) {
     .currentLap = 1,
     .firstPlaceDriverName = "",
     .firstPlaceRaceCarColor = ""
-  };
+  }; 
 
   for (int i = race.currentLap; i <= race.numberOfLaps; i++) {
     updateRaceCar(raceCar1);
@@ -116,7 +113,7 @@ void startRace(struct RaceCar* raceCar1, struct RaceCar* raceCar2) {
     printFirstPlaceAfterLap(race);
     race.currentLap += 1;
   } printCongratulation(race);
-}
+} // Starts the race
 
 int main() {
   srand(time(0));
@@ -131,10 +128,10 @@ int main() {
     .driverName = "Josh",
     .raceCarColor = "green",
     .totalLapTime = 0
-  };
+  }; // Information for driver 1 & 2
 
-  printIntro();
-  printCountDown();
-  startRace(&raceCar1, &raceCar2);
+  printIntro(); // Prints intro statement
+  printCountDown(); // Prints countdown
+  startRace(&raceCar1, &raceCar2); // Starts race with information from raceCar1 & 2
   
 };
